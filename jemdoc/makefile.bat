fcf1c8c48fc8c0fc4e86fc2589d0212a03e796c0
whkkkkkkc <1135712069@qq.com>
2020-05-17 20:02:16 +0800

    modified:   awards.html 	modified:   biography.html 	modified:   contact.html 	modified:   image/Your-image.png 	modified:   image/word.png 	modified:   index.html 	modified:   jemdoc/MENU 	modified:   jemdoc/awards.jemdoc 	modified:   jemdoc/biography.jemdoc 	modified:   jemdoc/contact.jemdoc 	modified:   jemdoc/index.jemdoc 	modified:   jemdoc/makefile.bat 	modified:   jemdoc/news.jemdoc 	modified:   news.html 	modified:   publications.html

--- jemdoc/makefile.bat
+++ jemdoc/makefile.bat
@@ -1,8 +1,8 @@
 python ./jemdoc.py -c custom.conf -o ../ index.jemdoc
 python ./jemdoc.py -o ../ biography.jemdoc
 python ./jemdoc.py -o ../ news.jemdoc
-python ./jemdoc.py -o ../ teaching.jemdoc
-python ./jemdoc.py -o ../ services.jemdoc
+::python ./jemdoc.py -o ../ teaching.jemdoc
+::python ./jemdoc.py -o ../ services.jemdoc
 python ./jemdoc.py -o ../ awards.jemdoc
 python ./jemdoc.py -o ../ contact.jemdoc
 python ../ref/bibconvert.py --suffix web --highlight "Y. Li" --highlight "Yongfu Li" --highlight "Li, Yongfu" --input ../ref/Top.bib --input ../ref/publications.bib --header publications_header.jemdoc > publications.jemdoc
